a4df845ed96e381c857ed2f873c5d6aa2d37fde2
nannipy <pernazza.1936239@studenti.uniroma1.it>
2025-12-15 15:35:50 +0100

    Remove static export and unoptimized image configurations, and refine remote image hostname pattern.

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**",
+        hostname: "*",
       },
     ],
   },
